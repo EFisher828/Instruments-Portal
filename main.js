992526a598bb2d9444765df1d8032e4e810964e1
EFisher828 <71984782+EFisher828@users.noreply.github.com>
2022-10-05 11:00:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const tempAndRHData = 'https://raw.githubusercontent.com/EFisher828/Instruments-Portal/main/tempAndRH-10-4.csv';
+const tempAndRHData = 'https://raw.githubusercontent.com/EFisher828/Instruments-Portal/main/tempAndRH-10-5.csv';
 function tempAndRH() {
     Highcharts.chart('tempAndRH', {
         chart: {
@@ -7,18 +7,6 @@ function tempAndRH() {
         },
         title: {
             text: 'Temperature and Dew Point'
-        },
-        accessibility: {
-            announceNewData: {
-                enabled: true,
-                minAnnounceInterval: 15000,
-                announcementFormatter: function (allSeries, newSeries, newPoint) {
-                    if (newPoint) {
-                        return 'New point added. Value: ' + newPoint.y;
-                    }
-                    return false;
-                }
-            }
         },
         data: {
             csvURL: tempAndRHData,
@@ -32,7 +20,7 @@ function tempAndRH() {
 }
 tempAndRH();
 
-const solarRadData = 'https://raw.githubusercontent.com/EFisher828/Instruments-Portal/main/solarRad-10-4.csv';
+const solarRadData = 'https://raw.githubusercontent.com/EFisher828/Instruments-Portal/main/solarRad-10-5.csv';
 function solarRad() {
     Highcharts.chart('solarRad', {
         chart: {
@@ -41,18 +29,6 @@ function solarRad() {
         },
         title: {
             text: 'Solar Radiation'
-        },
-        accessibility: {
-            announceNewData: {
-                enabled: true,
-                minAnnounceInterval: 15000,
-                announcementFormatter: function (allSeries, newSeries, newPoint) {
-                    if (newPoint) {
-                        return 'New point added. Value: ' + newPoint.y;
-                    }
-                    return false;
-                }
-            }
         },
         data: {
             csvURL: solarRadData,
